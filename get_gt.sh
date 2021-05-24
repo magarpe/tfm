d306fbbf42fec04bb240b5875fa17f4f2d30fbed
magarpe <marina.sofia.garcia@gmail.com>
2021-05-24 21:29:57 +0200

    sensor added

---
+++ get_gt.sh
@@ -0,0 +1,23 @@
+﻿#!/bin/bash
+
+## sh **.sh
+
+ 
+# this is a comment and is ignored
+<<COMMENT
+    This is a multiple line comment In Bash Scripting
+COMMENT
+
+
+#echo INITIALIZE ROSCORE
+#roscore
+#gnome-terminal -e "roscore"
+#sleep 2
+
+echo gazebo_sim and ps3 control -> connect ps3
+
+gnome-terminal \
+  --tab -e 'bash -c "rosbag play 2021-01-02-13-05-47.bag.active"' \
+  \
+  --tab -e 'bash -c "rostopic echo -p /ground_truth/state > gt.txt"'\
+
---
+++ keyb_ardrone.sh
@@ -0,0 +1,30 @@
+﻿#!/bin/bash
+
+## sh **.sh
+
+ 
+# this is a comment and is ignored
+<<COMMENT
+    This is a multiple line comment In Bash Scripting
+COMMENT
+
+
+#echo INITIALIZE ROSCORE
+#roscore
+#gnome-terminal -e "roscore"
+#sleep 2
+
+echo gazebo_sim and keyboard control, recording front image and ground truth
+
+gnome-terminal \
+  --tab -e 'bash -c "roslaunch tfm_gazebo maze.launch"' \
+  --tab -e 'bash -c "rosrun ardrone_tutorials keyboard_controller.py"'\
+  --tab -e 'bash -c "rosbag record /ardrone/front/image_raw /ground_truth/state"' 
+#  --tab -e 'bash -c "rosbag record /ardrone/front/image_raw"'
+#  --tab -e 'bash -c "kazam"'\
+#  \
+ 
+
+# rosbag record /ardrone/front/image_raw
+
+
